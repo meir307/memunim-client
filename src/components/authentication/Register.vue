16adb69e4396a44dffec88d9870022c804ad83c4
meir307 <62471736+meir307@users.noreply.github.com>
2025-07-22 09:50:48 +0300

    aaa

--- src/components/authentication/Register.vue
+++ src/components/authentication/Register.vue
@@ -1,93 +1,83 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" sm="8" md="4">
-        <v-card>
-          <v-card-title class="popup-title">
-            <h3>הרשמה</h3>
-          </v-card-title>
-          <v-card-text>
-            <!-- <v-form ref="form"> -->
-              <v-text-field
-                v-model="user.fullName"
-                :rules="validationRules.fullNameRules"
-                label="שם מלא"
-                type="text"
-                reverse
-              ></v-text-field>
-              <v-text-field
-                v-model="user.email"
-                :rules="validationRules.emailRules"
-                label="אימייל"
-                type="email"
-                reverse
-                
-              ></v-text-field>
-              <v-text-field 
-                v-model="user.phone"  
-                :rules="validationRules.phoneRules"
-                label="טלפון" type="text"  
-                reverse>
-             </v-text-field>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" sm="8" md="4">
+                <v-card>
+                    <v-card-title class="popup-title">
+                        <h3>הרשמה</h3>
+                    </v-card-title>
+                    <v-card-text>
+                        <!-- <v-form ref="form"> -->
+                        <v-text-field v-model="user.fullName" :rules="validationRules.fullNameRules" label="שם מלא"
+                            type="text" reverse></v-text-field>
+                        <v-text-field v-model="user.email" :rules="validationRules.emailRules" label="אימייל"
+                            type="email" reverse></v-text-field>
+                        <v-text-field v-model="user.phone" :rules="validationRules.phoneRules" label="טלפון" type="text"
+                            reverse>
+                        </v-text-field>
 
 
-              <v-text-field 
-                v-model="user.password" 
-                 :rules="validationRules.passwordRules"
-                label="סיסמה" 
-                type="password" 
-                reverse>
-            </v-text-field>
-              
-              
-              
-              <div class="popup-btn-row">
-                <v-btn @click="regUser" color="primary">הרשם</v-btn>
-                <v-btn @click="$emit('btnCancel')">סגור</v-btn>
-                <v-spacer></v-spacer>
-              </div>
-            <!-- </v-form> -->
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+                        <v-text-field v-model="user.password" :rules="validationRules.passwordRules" label="סיסמה"
+                            type="password" reverse>
+                        </v-text-field>
+
+
+
+                        <div class="popup-btn-row">
+                            <v-btn @click="regUser" color="primary">הרשם</v-btn>
+                            <v-btn @click="$emit('btnCancel')">סגור</v-btn>
+                            <v-spacer></v-spacer>
+                        </div>
+                        <!-- </v-form> -->
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
- import * as validationRules from '@/utils/ValidationRules'
+import * as validationRules from '@/utils/ValidationRules'
+import axios from 'axios'
+import { useCommonStore } from '@/stores/CommonStore'
+
 export default {
-  name: 'RegistrationComponent',
-  data() {
-    return {
-      user: {
-        fullName: '',
-        email: '',
-        phone: '',
-        password: ''
-      },
-      validationRules,
-      
+    name: 'RegistrationComponent',
+    data() {
+        return {
+            user: {
+                fullName: '',
+                email: '',
+                phone: '',
+                password: ''
+            },
+            validationRules,
+
+        }
+    },
+    methods: {
+        async regUser() {
+            const emailValid = this.validationRules.emailRules.every(rule => rule(this.user.email) === true);
+            const passwordValid = this.validationRules.passwordRules.every(rule => rule(this.user.password) === true);
+            const fullNameValid = this.validationRules.fullNameRules.every(rule => rule(this.user.fullName) === true);
+            const phoneValid = this.validationRules.phoneRules.every(rule => rule(this.user.phone) === true);
+
+            if (emailValid && passwordValid && fullNameValid && phoneValid) {
+                try {
+                    const commonStore = useCommonStore()
+                    const response = await axios.post(commonStore.apiUrl + 'user/register', this.user)
+                    alert('Registration successful!')
+                    console.log(response)
+                    this.$emit('btnCancel')
+                } catch (error) {
+                    alert(error.response?.data?.message || 'Registration failed')
+                }
+            } else {
+                alert('not valid')
+            }
+        }
     }
-  },
-  methods: {
-    regUser() {
-        const emailValid = this.validationRules.emailRules.every(rule => rule(this.user.email) === true);
-        const passwordValid = this.validationRules.passwordRules.every(rule => rule(this.user.password) === true);
-        const fullNameValid = this.validationRules.fullNameRules.every(rule => rule(this.user.fullName) === true);
-        const phoneValid = this.validationRules.phoneRules.every(rule => rule(this.user.phone) === true);
-        
-        if (emailValid && passwordValid && fullNameValid && phoneValid)
-            alert('valid')
-        else
-            alert('not valid')
-    
-        
-    }
-  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
